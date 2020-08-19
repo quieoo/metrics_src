0df2dfa5210aa95559f9bc3cfc5ac2196572bd9a
quieoo <quieoob@gmail.com>
2020-08-19 15:11:10 +0800

    init

---
+++ src/main/java/com/embedGroup/metrics/App.java
@@ -0,0 +1,13 @@
+package com.embedGroup.metrics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/embedGroup/metrics/Metric.java
@@ -0,0 +1,8 @@
+package com.embedGroup.metrics;
+
+/**
+ * A tag interface to indicate that a class is a metric.
+ */
+public interface Metric {
+
+}
